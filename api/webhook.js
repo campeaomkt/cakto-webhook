937896bbcfc4ebac394a13a5cc917222ca3042c8
Eliab Campos <campeao.mkt.user@gmail.com>
2026-02-23 16:35:21 -0400

    ajuste webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,17 @@
-export default function handler(req, res) {
-  console.log("Método:", req.method);
-  console.log("Body recebido:", req.body);
+export default async function (req, res) {
+  try {
+    console.log("Método:", req.method);
 
-  return res.status(200).json({ ok: true });
+    if (req.method !== "POST") {
+      return res.status(200).json({ ok: true });
+    }
+
+    console.log("Body recebido:", req.body);
+
+    return res.status(200).json({ recebido: true });
+
+  } catch (error) {
+    console.error("Erro:", error);
+    return res.status(500).json({ erro: "Erro interno" });
+  }
 }
