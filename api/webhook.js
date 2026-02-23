976c0168caaafe5eefca679f3bfdcb6acbb060be
Eliab Campos <campeao.mkt.user@gmail.com>
2026-02-23 16:37:15 -0400

    ajuste v2 webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,17 +1,26 @@
 export default async function (req, res) {
   try {
-    console.log("Método:", req.method);
 
     if (req.method !== "POST") {
       return res.status(200).json({ ok: true });
     }
 
-    console.log("Body recebido:", req.body);
+    // 🔥 Lê o body manualmente
+    let body = "";
+
+    for await (const chunk of req) {
+      body += chunk;
+    }
+
+    const json = JSON.parse(body);
+
+    console.log("Webhook recebido:");
+    console.log(json);
 
     return res.status(200).json({ recebido: true });
 
   } catch (error) {
     console.error("Erro:", error);
-    return res.status(500).json({ erro: "Erro interno" });
+    return res.status(200).json({ ok: true });
   }
 }
